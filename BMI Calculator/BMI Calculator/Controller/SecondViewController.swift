00ea6b58a80fb038e18c30b2823e8f728865a7b9
Tushar Vijayvargiya <65571665+tusharvijay24@users.noreply.github.com>
2021-06-29 03:19:06 +0530

    Add files via upload

---
+++ BMI Calculator/BMI Calculator/Controller/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Tushar Vijayvargiya on 28/06/21.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ BMI Calculator/BMI Calculator/Models/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Tushar Vijayvargiya on 29/06/21.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
